392261387f01f67bb5958ece94f8f7d57538312d
Thomas <thomas@glirc.dk>
2019-05-08 11:20:58 +0100

    auto_2019-05-08_12:20

--- LiveCode2019/Program.cs
+++ LiveCode2019/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -34,6 +35,8 @@ namespace LiveCode2019
 
         static void Main(string[] args)
         {
+
+            Console.ReadKey();
             List<GameObject> objects = new List<GameObject>(){new Buggy(), new Tank()};
         }
     }
